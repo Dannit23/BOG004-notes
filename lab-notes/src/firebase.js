2cdda47787eeb5ea7036da3f9857028fb87c774b
Dannit23 <danalexa23@gmail.com>
2022-05-03 18:33:47 -0500

    se crean archivos para conectar el proyecto con react y firebase

---
+++ lab-notes/src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwvZXfKVtrdP19irsje5hzAc3MngO_-VM",
+  authDomain: "lab-notes-6f68f.firebaseapp.com",
+  projectId: "lab-notes-6f68f",
+  storageBucket: "lab-notes-6f68f.appspot.com",
+  messagingSenderId: "304698835194",
+  appId: "1:304698835194:web:c5ee55cc7b5b6f53724943",
+  measurementId: "G-3HZDX9HH1J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = app.firestore();
