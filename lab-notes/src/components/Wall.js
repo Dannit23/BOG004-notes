7653546c46518fef2da0d672a26720038b97e99f
Dannit23 <danalexa23@gmail.com>
2022-05-11 20:36:46 -0500

    Se realiza la funcionalidad del botón cerrar sesión en el componente Wall

--- lab-notes/src/components/Wall.js
+++ lab-notes/src/components/Wall.js
@@ -1,25 +1,39 @@
 import React from 'react'
+import { Navigate, useNavigate } from "react-router-dom"; 
 
 const Wall = () => {
-    return (
-        <div id="view-wall">
-            <div id="outset">
-              <img className='image-notesW' src="https://i.imgur.com/Wbmj6wv.png" alt="notas"></img> 
-              <img className='logoW' src="https://i.imgur.com/B3HZxcf.png" alt="logo"></img>
-              <img className='add-note' src="https://i.imgur.com/PrK7Qax.png" alt="agregarNota"></img>
-              <img className='sign-off' src="https://i.imgur.com/kkKxJhI.png" alt="cerrarSesion"></img>
-            </div>             
-            <div id="user-profile">
-              <img className='image-profile' src={localStorage.getItem("profilePic")} />
-              <div id="user-data">
-                  <h1>{localStorage.getItem("name")}</h1>
-                  <h1>{localStorage.getItem("email")}</h1>
-              </div>             
-            </div>
+  let Navigate = useNavigate()
+  function LogoutButton(e) {
+    e.preventDefault();
+    console.log('click')
+    Navigate("/")
+  } 
+
+  return (
+    <div id="view-wall">
+      <div id="outset">
+        <img className='image-notesW' src="https://i.imgur.com/Wbmj6wv.png" alt="notas"></img> 
+        <img className='logoW' src="https://i.imgur.com/B3HZxcf.png" alt="logo"></img>
+        <img className='add-note' src="https://i.imgur.com/PrK7Qax.png" alt="agregarNota"></img>
+        <form onSubmit={LogoutButton}>
+          <button type='submit' >
+            <img className='sign-off' src="https://i.imgur.com/kkKxJhI.png" alt="cerrarSesion"></img>
+          </button>
+        </form>
+      </div>             
+      <div id="user-profile">
+        <img className='image-profile' src={localStorage.getItem("profilePic")} />
+        <div id="user-data">
+          <h1>{localStorage.getItem("name")}</h1>
+          <h1>{localStorage.getItem("email")}</h1>
+        </div>             
+      </div>
                     
-        </div>
+    </div>
       
-    )
+  )
+  
 }
 
 export default Wall
+
