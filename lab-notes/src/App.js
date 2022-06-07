fc59fcdff47dd0774b5b6428c2574fe56d935c21
Dannit23 <danalexa23@gmail.com>
2022-06-07 17:30:30 -0500

    Se ajustan propiedades de css para dispositivo tablet

--- lab-notes/src/App.js
+++ lab-notes/src/App.js
@@ -12,18 +12,10 @@ const NotFound = () => <h1>NotFound</h1>
 function App() {
   return (
     <div className='container'>
-      <header>
-          <nav>
-            <ul> 
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/wall'>Wall</Link></li>
-            </ul>
-          </nav>
-      </header>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Wall' element={<Wall />} />
-          <Route path="*" element={<NotFound />} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+         <Route path='/Wall' element={<Wall />} />
+         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
      
